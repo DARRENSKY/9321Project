ce012499661f4bc1fe6ce042bd8bd2ee242532c0
darren <32636300+DARRENSKY@users.noreply.github.com>
2018-05-23 14:43:24 +1000

    Add files via upload

---
+++ lga_nsw.py
@@ -0,0 +1,5 @@
+lga_list=["Ashfield","Botany Bay","Lane Cove","Leichhardt","Marrickville","Mosman","North Sydney","Randwick","Sydney","Waverley",
+          "Woollahra","Auburn","Bankstown","Burwood","Canterbury","Canada Bay","Hunters Hill","Hurstville","Kogarah","Ku-Ring-Gai","Manly"
+          ,"Parramatta","Rockdale","Ryde","Strathfield","Willoughby","The Hills Shire","Blacktown","Blue Mountains","Camden","Campbelltown"
+          ,"Fairfield","Gosford","Hawkesbury","Holroyd","Hornsby","Liverpool","Penrith","Pittwater","Sutherland Shire","Warringah"
+          ,"Wollondilly","Wyong","Cessnock","Kiama","Lake Macquarie","Maitland","Newcastle","Port Stephens","Shellharbour","Wollongong","NSW"]
